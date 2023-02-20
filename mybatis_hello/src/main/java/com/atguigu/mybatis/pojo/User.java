8a167838198be868eb739f93ea03288c764a7de6
vaemusician <172910307@qq.com>
2023-02-20 23:08:08 +0800

    home commit

---
+++ mybatis_hello/src/main/java/com/atguigu/mybatis/pojo/User.java
@@ -0,0 +1,20 @@
+package com.atguigu.mybatis.pojo;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer age;
+
+    private String gender;
+
+    private String emil;
+
+
+}
+
+
+
