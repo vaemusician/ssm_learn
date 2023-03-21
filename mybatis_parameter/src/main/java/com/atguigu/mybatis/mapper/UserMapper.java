39a6c97b6b342e860168b90e84904849b288019c
vaemusician <172910307@qq.com>
2023-03-21 22:12:29 +0800

    home commit

---
+++ mybatis_parameter/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.User;
+
+public interface UserMapper {
+    // 根据用户名查询用户信息
+    User getUserByUsername(String username);
+
+}
---
+++ mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.mapper.UserMapper;
+import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ParameterTest {
+    @Test
+    public void testGetUserByUsername() throws IOException {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserByUsername("admin0306");
+        System.out.println(user);
+
+    }
+}
