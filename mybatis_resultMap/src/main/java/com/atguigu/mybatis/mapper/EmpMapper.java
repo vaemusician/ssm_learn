344f604cfbff9886e31fddd5242da6092404b1bf
vaemusician <172910307@qq.com>
2023-04-06 17:14:46 +0800

    mac commit

--- mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
+++ mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -1,5 +1,9 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 public interface EmpMapper {
+    Emp getEmpByEmpId(@Param("EmpId") Integer EmpId);
 
 }
---
+++ mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -0,0 +1,17 @@
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ResultMapTest {
+    @Test
+    public void testGetEmpByEmpId() throws IOException {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpByEmpId(1);
+        System.out.println(emp);
+    }
+}
